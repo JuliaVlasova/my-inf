16a64d3ec483d921be2e709fca01ac88047c62f7
JuliaVlasova <juliavlasova10111989@gmail.com>
2024-09-01 13:50:24 +0300

    scss

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,6 @@ import ArseneLupinLegs from './components/arsene-lupin-image/ArseneLupinLegs.vue
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <ArseneLupinHead />
       <ArseneLupinBody />
