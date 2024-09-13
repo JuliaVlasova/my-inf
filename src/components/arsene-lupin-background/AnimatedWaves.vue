f7e632206ffefdd87f2a0b5100450fb144262410
JuliaVlasova <juliavlasova10111989@gmail.com>
2024-09-13 13:43:19 +0300

    waves

--- src/components/arsene-lupin-background/AnimatedWaves.vue
+++ src/components/arsene-lupin-background/AnimatedWaves.vue
@@ -1,15 +1,15 @@
 <script setup></script>
 
 <template>
-  <div class="waveWrapper waveAnimation">
-    <div class="waveWrapperInner bgTop">
-      <div class="wave waveTop" style=""></div>
+  <div class="wave-wrapper wave-wrapper_animated">
+    <div class="wave-wrapper__inner wave-wrapper__inner_top">
+      <div class="wave wave_top"></div>
     </div>
-    <div class="waveWrapperInner bgMiddle">
-      <div class="wave waveMiddle"></div>
+    <div class="wave-wrapper__inner wave-wrapper__inner_middle">
+      <div class="wave wave_middle"></div>
     </div>
-    <div class="waveWrapperInner bgBottom">
-      <div class="wave waveBottom"></div>
+    <div class="wave-wrapper__inner wave-wrapper__inner_bottom">
+      <div class="wave wave_bottom"></div>
     </div>
   </div>
 </template>
