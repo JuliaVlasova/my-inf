224c4784dcc5142973681c2d6e44746edae50e9a
JuliaVlasova <juliavlasova10111989@gmail.com>
2025-04-08 17:03:47 +0300

    more random answers

---
+++ src/assets/js/arsene-lupin-spy.js
@@ -0,0 +1,16 @@
+export function ArseneLupinSpy() {
+  const lupin = document.getElementById('arsene-lupin')
+
+
+
+
+
+}
+
+/* Если с инфом не говорят больше 2000ms, то у него включается режим слежки за мышкой. 
+Используй тригонометрические функции в CSS, чтобы создать плавный ступенчатый переход с задержкой
+.character {
+transition-delay:
+calc(sin((var(--index) / 12) * 45deg) * 0.475s);
+}
+ */
